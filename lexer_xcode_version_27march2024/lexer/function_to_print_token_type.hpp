87a1234053c0a554565b81c293927626cf7208ee
Mohamed Raslan <46673407+mohamedmahmoud95@users.noreply.github.com>
2024-03-27 17:01:19 +0200

    Update function_to_print_token_type.hpp

--- lexer_xcode_version_27march2024/lexer/function_to_print_token_type.hpp
+++ lexer_xcode_version_27march2024/lexer/function_to_print_token_type.hpp
@@ -1,9 +1,4 @@
-//
-//  function_to_print_token_type.hpp
-//  lex
-//
-//  Created by Mohamed Raslan on 27/03/2024.
-//
+
 
 #ifndef function_to_print_token_type_hpp
 #define function_to_print_token_type_hpp
