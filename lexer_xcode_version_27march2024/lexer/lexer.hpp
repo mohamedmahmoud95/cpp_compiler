7dcc39fa6ac86ae963e0861f3d6cac8adea6930f
Mohamed Raslan <46673407+mohamedmahmoud95@users.noreply.github.com>
2024-03-27 16:59:54 +0200

    Add files via upload

---
+++ lexer_xcode_version_27march2024/lexer/lexer.hpp
@@ -0,0 +1,46 @@
+//
+//  lexer.hpp
+//  lex
+//
+//  Created by Mohamed Raslan on 27/03/2024.
+//
+
+#ifndef lexer_hpp
+#define lexer_hpp
+
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <regex>
+#include <vector>
+#include "token_types.h"
+
+struct Token {
+    TokenType type;
+    std::string lexeme;
+};
+
+
+
+class Lexer {
+public:
+    Lexer(const std::string& input);
+    Token getNextToken();
+    
+private:
+    const std::string input;
+    size_t position;
+    std::string currentBuffer;
+    std::string nextBuffer;
+    const size_t bufferSize;
+    
+    void swapBuffers();
+    
+    std::string extractLexeme(const std::string& buffer);
+    
+    bool isDelimiter(char c);
+    
+    void handleToken(Token& token);
+};
+
+#endif /* lexer_hpp */
