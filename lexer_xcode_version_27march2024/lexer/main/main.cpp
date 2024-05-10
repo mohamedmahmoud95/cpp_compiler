e808c4b495ecbff3599aa21a509dd6f99a5774aa
Mohamed Raslan <46673407+mohamedmahmoud95@users.noreply.github.com>
2024-05-11 01:15:58 +0300

    Update main.cpp, more organized, more modular

--- lexer_xcode_version_27march2024/lexer/main/main.cpp
+++ lexer_xcode_version_27march2024/lexer/main/main.cpp
@@ -13,38 +13,31 @@ vector <string> identifiers;
 set<string> listOfUniqeIdentifiers;
 void extract_symbol_table_from_token_stream(vector<string> identifiers);
 void print_symbol_table();
+string readSourceCode(const string& filename);
+string getHardCoddedSourceCodeExample();
+
+// File to read the source code from
+//If it says: "Error opening file: ", you probably need to change the path to the path where the file exists on your machine
+string sourse_code_file_name = "/Users/mohamedraslan/Downloads/cpp_compiler-main/lexer_xcode_version_27march2024/lexer/input.txt";
+
+string token_stream_output_file_name = "/Users/mohamedraslan/Downloads/cpp_compiler-main/lexer_xcode_version_27march2024/lexer/token_stream.txt";
+
+string symbol_table_output_file_name = "/Users/mohamedraslan/Downloads/cpp_compiler-main/lexer_xcode_version_27march2024/lexer/symbol_table.txt";
 
 int main() {
     
-    // File to read the source code from
-    //If it says: "Error opening file: ", you probably need to change the path to the path where the file exists on your machine
-    string filename = "/Users/mohamedraslan/Downloads/cpp_compiler-main/lexer_xcode_version_27march2024/lexer/input.txt";
+    string inputString = readSourceCode(sourse_code_file_name);
 
-
-    ifstream inputFile(filename);
-
-    // Error handling: Check if the file opened successfully
-    if (!inputFile.is_open()) {
-      cerr << "Error opening file: " << filename << endl;
-      return 1;
-    }
-
-    // Read the entire file content into a string
-    string inputString;
-    getline(inputFile, inputString, static_cast<char>(EOF));
+    //or, use the following hard codded source code example:
+    //string inputString = getHardCoddedSourceCodeExample();
     
-    inputFile.close();
-
+    
    //  Create the lexer with the input string
        Lexer lexer(inputString);
 
-    // Sample input string
-//    string input = "bool private public protected class template :: 3.14e5 if x == y return 1; else return 0; while _cycle = 9; int frawla = 2098 ; int = --8++ ; [] [ ]  float zrka23elyamama zrka23elyamama zrka23elyamama = 0.221; 0b1010 12.345 0xABCD 0777 ;Ibrahim !Donia && ||; ()) === != % > < >= 8.98-- *= %= += -= <<= ^= ?: &= + - () [] {} \\ ? . , ; : # \n ' \" & | ^ ~";
-//
-//    Lexer lexer(input);
     Token token;
 
-    ofstream tokenStreamFile("/Users/mohamedraslan/Downloads/cpp_compiler-main/lexer_xcode_version_27march2024/lexer/token_stream.txt");
+    ofstream tokenStreamFile(token_stream_output_file_name);
 
     do {
         token = lexer.getNextToken();
@@ -52,6 +45,8 @@ int main() {
         cout <<  "Token: " <<setw(10) << left << token.lexeme << setw(10) << right << "Type: " <<tokenTypeNames[token.type]<< endl;
 
         tokenStreamFile << "Token: " <<setw(10) << left << token.lexeme << setw(10) << right << "Type: " <<tokenTypeNames[token.type]<< endl;
+        
+        //match identifier, if matched, add to the identifiers list, to be used later to extract symbol table
         if((isalpha(token.lexeme[0]) || token.lexeme[0] == '_')){
             identifiers.push_back(token.lexeme);
         }
@@ -68,6 +63,28 @@ int main() {
     return 0;
 }
 
+// Function to read source code from a file into a string
+string readSourceCode(const string& filename) {
+    ifstream inputFile(filename);
+
+    // Error handling: Check if the file opened successfully
+    if (!inputFile.is_open()) {
+        cerr << "Error opening file: " << filename << endl;
+               exit(1);
+    }
+
+    // Read the entire file content into a string
+    string inputString;
+    getline(inputFile, inputString, static_cast<char>(EOF));
+    
+    inputFile.close();
+    
+    return inputString;
+}
+
+string getHardCoddedSourceCodeExample(){
+    return "bool private public protected class template :: 3.14e5 if x == y return 1; else return 0; while _cycle = 9; int frawla = 2098 ; int = --8++ ; [] [ ]  float zrka23elyamama zrka23elyamama zrka23elyamama = 0.221; 0b1010 12.345 0xABCD 0777 ;Ibrahim !Donia && ||; ()) === != % > < >= 8.98-- *= %= += -= <<= ^= ?: &= + - () [] {} \\ ? . , ; : # \n ' \" & | ^ ~";
+}
 
 void extract_symbol_table_from_token_stream(vector<string> identifiers){
     // Insert elements from the vector into the set (duplicates will be automatically removed)
@@ -78,7 +95,7 @@ void extract_symbol_table_from_token_stream(vector<string> identifiers){
 }
 
 void print_symbol_table(){
-    ofstream symbolTableFile("/Users/mohamedraslan/Downloads/cpp_compiler-main/lexer_xcode_version_27march2024/lexer/symbol_table.txt");
+    ofstream symbolTableFile(symbol_table_output_file_name);
 
     cout<<"\n\n================================================\n";
     cout<<"printing sybmol table:\n";
@@ -89,6 +106,4 @@ void print_symbol_table(){
     }
     cout<<"================================================\n";
     symbolTableFile.close();
-    
-
 }
